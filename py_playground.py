c122c9d1017b57cb9164dfc7efdaf4a527c58c7c
hyungjunk <khjgd2@gmail.com>
2019-02-18 10:58:08 +0900

    Update py_playground.py

--- py_playground.py
+++ py_playground.py
@@ -484,3 +484,17 @@ load_dotenv(dotenv_path=env_path)
 print(os.getenv("HELLO"))
 
 #.env 파일은 HELLO=WORLD
+
+
+# Nest Dict
+class NestedDict(dict):
+    def __getitem__(self, key):
+        if key in self:
+            return self.get(key)
+        return self.setdefault(key, NestedDict())
+
+    def __add__(self, other):
+        return other
+
+    def __sub__(self, other):
+        return other
